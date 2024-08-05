83866e61e9876345b78d96b6e3932844f68b12a1
CTC97 <pixelq6@gmail.com>
2024-08-04 22:35:13 -0400

    ui overhaul

--- gnuddha.sh
+++ gnuddha.sh
@@ -13,13 +13,13 @@ SESSION_TIME=0
 BY_NAME=""
 QUOTE=""
 
+FLAG_F=false
 FRAME_RATE=24
+
 FRAME_UP=1
 FRAME_INDEX=1
 SLEEP_DUR=$(echo "scale=2; 1 / $FRAME_RATE" | bc)
 # echo "DUR : ${SLEEP_DUR}"
-
-
 
 # Function to handle window resize
 handle_resize() {
@@ -37,16 +37,45 @@ cleanup() {
 }
 trap cleanup EXIT
 
-# Function to fetch a random quote
 fetchQuote() {
     api_data=$(curl -s "https://buddha-api.com/api/random")
     BY_NAME=$(echo "$api_data" | jq -r '.byName')
     QUOTE=$(echo "$api_data" | jq -r '.text')
 }
 
+splitQuote() {
+    words=($QUOTE)
+    local max_length=24
+
+    lines=()
+    local result=""
+
+    for word in ${words[@]}; do 
+        length_test=$(( ${#result} + ${#word} + 1 ))  # +1 for space between words
+
+        if (( length_test > max_length )); then 
+            lines+=("$result")
+            echo -e "\t\t${result}"
+            result="$word"  # Start new line with the current word
+        else 
+            if [[ -n $result ]]; then
+                result+=$' '$word  # Add space between words
+            else
+                result=$word
+            fi
+        fi 
+    done
+
+    if [[ -n $result ]]; then
+        lines+=("$result")
+        echo -e "\t\t${result}"
+    fi
+}
+
 # Function to iterate and display content
 iterate() {
     clear
+    echo -e "\n\n"
     if (( FRAME_INDEX == 11 )); then
         FRAME_UP=0
     fi
@@ -61,12 +90,45 @@ iterate() {
         FRAME_INDEX=$((FRAME_INDEX-1))
     fi
     
-    frame=$((FRAME_INDEX % 12))
-    cat "b_frames/${frame}.txt"
-    echo -e "\n${QUOTE}" | fold -w 32 -s
-    echo -e "\t- ${BY_NAME}"
     TOTAL=$(echo "scale=2; $TOTAL + $SLEEP_DUR" | bc)
-    echo -e "\n\nTotal time: ${TOTAL} seconds" 
+
+    frame=$((FRAME_INDEX % 12))
+    line_index=0
+
+    local last_quote_line=444
+
+    while frame_file= read -r line; do
+        if ((line_index == 3)); then
+            echo -e "$line\tTotal time: ${TOTAL} seconds"
+        elif ((line_index == 5)); then
+            echo -e "$line\t${lines[0]}"
+            last_quote_line=5
+        elif ((line_index == 6 && ${#lines[@]} >= 1)); then
+            echo -e "$line\t${lines[1]}"
+            last_quote_line=6
+        elif ((line_index == 7 && ${#lines[@]} >= 2)); then
+            echo -e "$line\t${lines[2]}"
+            last_quote_line=7
+        elif ((line_index == 8 && ${#lines[@]} >= 3)); then
+            echo -e "$line\t${lines[3]}"
+            last_quote_line=8
+        elif ((line_index == 9 && ${#lines[@]} >= 4)); then
+            echo -e "$line\t${lines[4]}"
+            last_quote_line=9
+        elif ((line_index == 10 && ${#lines[@]} >= 5)); then
+            echo -e "$line\t${lines[5]}"
+            last_quote_line=10
+         elif ((line_index == 11 && ${#lines[@]} >= 6)); then
+            echo -e "$line\t${lines[6]}"
+            last_quote_line=11
+        elif ((line_index==last_quote_line+1)); then 
+            echo -e "$line\t${BY_NAME}"
+        else
+            echo -e "$line"
+        fi
+        ((line_index++))
+    done < "b_frames/${frame}.txt"
+    echo -e "\n"
     sleep "$SLEEP_DUR"
     clear
 }
@@ -78,30 +140,47 @@ usage() {
 }
 
 # Parsing command-line options
-while getopts ":t:v" opt; do
+while getopts ":t:f:" opt; do
     case ${opt} in
         t )
             FLAG_T=true
             SESSION_TIME=$OPTARG
             echo "Option -t set with value: $SESSION_TIME"
             ;;
+        f )
+            FLAG_F=true
+            FRAME_RATE=$OPTARG
+            echo "Option -f set with value: $FRAME_RATE"
+            ;;
         \? )
             echo "Invalid option: -$OPTARG"
+            usage
+            ;;
+        : )
+            echo "Option -$OPTARG requires an argument."
             usage
             ;;
     esac
 done
 shift $((OPTIND -1))
 
-# Fetch the initial quote
+SLEEP_DUR=$(echo "scale=2; 1 / $FRAME_RATE" | bc)
+
+# Fetch the initial quote and format
 fetchQuote
+splitQuote
+
+for line in "${lines[@]}"; do
+    echo -e "linehere\n\n$line\n\n"
+done
+
+if [ "$FLAG_F" = true ]; then 
+    echo FLAGFFRUE
+    echo "$FRAME_RATE"
+fi
 
 # Main logic
 if [ "$FLAG_T" = true ]; then
-    echo "t: $TOTAL"
-    echo "st: $SESSION_TIME"
-
-    echo HERE
     
     if (( "$TOTAL < $((SESSION_TIME * 60))" | bc -l )); then echo "passed" 
     fi
